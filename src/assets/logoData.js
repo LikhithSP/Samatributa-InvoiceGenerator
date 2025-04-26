3bc2da82b33933962133c55989aa1eeb8128a170
Likhith SP <splikith04@gmail.com>
2025-04-26 23:58:57 +0530

    changed

--- src/assets/logoData.js
+++ src/assets/logoData.js
@@ -1,5 +1,5 @@
 // Default logo for the application (base64 encoded)
-export const defaultLogo = "dta:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAMgAAADICAYAAACtWK6eAAAABHNCSVQICAgIfAhkiAAAAAlwSFlzAAALEwAACxMBAJqcGAAADnVJREFUeJzt3XmUVNWdwPHvrarqBpruhm6gWWQHEVkUF1TABXdFE43RmMk4ZuKWOaqZnGMmmSWOmZjEzWSMccx4YuISUYlKokRc0LiAsgiyC0KzyNJssrV019161747mYIik6VfV9W9Ve/3+Qc5/d5773fgnLp169Y7YIwxxhhjjDHGGGOMMcYYY4wxxhhjjDHGGGOMMcYYY8YmN9odMD0zffr08qKioiOBaY7jTHZdtwwoACqBEiDXcZwy/*...many more encoded characters...*/";
+export const defaultLogo = ".images/logo.png";
 
 // Sama Tributa Solutions logo
 export const companyName = "Sama Tributa Solutions";
