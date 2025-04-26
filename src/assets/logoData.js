e7232286d4626a1777a36e0c70705242be9f382b
Likhith SP <splikith04@gmail.com>
2025-04-27 00:07:21 +0530

    comitted

--- src/assets/logoData.js
+++ src/assets/logoData.js
@@ -1,5 +1,5 @@
-// Default logo for the application (base64 encoded)
-export const defaultLogo = "images/favicon.png";
+// #ult logo for the application (base64 encoded)
+export const defaultLogo = "/images/favicon.png";
 
 // Sama Tributa Solutions logo
 export const companyName = "Sama Tributa Solutions";
