cf0a54870055af3d95bc4aa6251908a20322c300
Likhith SP <splikith04@gmail.com>
2025-05-09 22:03:51 +0530

    made changes

--- src/utils/supabaseClient.js
+++ src/utils/supabaseClient.js
@@ -1,7 +1,7 @@
 // src/utils/supabaseClient.js
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || process.env.SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || process.env.SUPABASE_ANON_KEY;
+const supabaseUrl = "https://enkcmybhwzayjwrheqzt.supabase.co";
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVua2NteWJod3pheWp3cmhlcXp0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY4MDA4MTMsImV4cCI6MjA2MjM3NjgxM30.z7_hdK3jT2x6JBgJZwXYrxF_qJf7M0GH3lRXrHyHJ-U";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
